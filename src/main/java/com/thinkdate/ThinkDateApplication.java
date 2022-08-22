cd8b45749bdd32821050e69fd39674a53b6486c2
mixheaven <mixheaven@hotmail.com>
2022-08-22 17:36:38 +0200

    Initial commit

---
+++ src/main/java/com/thinkdate/ThinkDateApplication.java
@@ -0,0 +1,13 @@
+package com.thinkdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkDateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkDateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thinkdate/ThinkDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkDateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
